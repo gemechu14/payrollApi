793bcbb37817cba6daa35a3b8115d243f2b9fd50
Gemechu Bulti Firisa <gemechubf@coopbank.local>
2023-05-12 14:22:11 +0300

    rrt

--- api/routes/Allowance.js
+++ api/routes/Allowance.js
@@ -4,40 +4,43 @@ const allowanceController = require('../controllers/Allowance.js');
 const middleware=require('../middleware/auth.js')
 //ADD ALLOWANCE
 router.post('/',
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
 allowanceController.add_Allowance);
 
 //CREATE
 router.post('/:employeeId',
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'), 
  allowanceController.Create_Allowances);
 //UPDATE
 router.put('/:id', 
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
 allowanceController.updateAllowance),
   ///DELETE
 router.delete('/:employeeId/:id', 
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
 allowanceController.delete_Allowances);
 
 //GET ONE
 router.get('/:id',
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
 allowanceController.get_single_Allowance);
 //GET ALL 
 router.get('/', 
 
-middleware.protect,
-middleware.restrictTo('Companyadmin'),
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
 allowanceController.get_All_Allowance);
 
 //ADD EXISTING ALLOWANCE TO EMPLOYEE
-router.post('/:employeeId/:allowanceId',allowanceController.addExistingAllowances);
+router.post('/:employeeId/:allowanceId',
+  middleware.protectAll,
+  middleware.restrictToAll('Companyadmin'),
+allowanceController.addExistingAllowances);
 
 //DELETE Allowance
 router.delete('/',
